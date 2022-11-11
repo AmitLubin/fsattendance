7cbec4eb19ff7b34dcf241e2cba213fd3645168b
Yona Bloy <107877894+natibloy@users.noreply.github.com>
2022-11-11 18:21:22 +0200

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 # first and foremost - input checking!
 # check if correct number of arguments were entered:
 if [ "$#" -ne 1 ]; then
@@ -16,17 +16,17 @@ echo "deploying to ${machine} machine..."
 # making sure final-project directory exists and copying to it all the files from the git repository:
 rsync -zrv --delete /var/lib/jenkins/workspace/attendance-project/ $machine:/home/ec2-user/final-project/
 # connecting to the input machine and running multiple commands:
-ssh $machine << EOF
+ssh $machine << 'EOF'
 	cd final-project/
 	bash docker-clean.sh
 	docker-compose up -d
 	sleep(20) 
-EOF
+'EOF'
 # if deploying to test move tests directory to test machine and run tests:
 if [ $machine == "test" ]; then
-	ssh test << EOF
+	ssh test << 'EOF'
 		cd final-project/tests/
 		bash test-back.sh
 		bash test-front.sh
-	EOF
+	'EOF'
 fi
