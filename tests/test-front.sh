c9e401d25a217adf39a3fd82bb87571cff6be6bc
Yona Bloy <natibloy@gmail.com>
2022-11-14 18:04:47 +0200

    fixed tests and changed dockerhub

--- tests/test-front.sh
+++ tests/test-front.sh
@@ -3,7 +3,7 @@
 echo "Running test on frontend..."
 url=localhost:3000
 code=$(curl -s -I ${url} | grep HTTP | awk '{print $2}')
-if [ code -eq 200 ]; then
+if [ $code -eq 200 ]; then
     echo "Test passed!"
 else
     echo "Test failed with status code: ${code}" >&2
