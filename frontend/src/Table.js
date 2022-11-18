e1828eb7b831148c4547dd29f0b8da25da9aeb8a
AmitLubin <amit.lubin@gmail.com>
2022-11-18 22:30:00 +0200

    scroll bar is not fixed but rest of the frontend is perfect

--- frontend/src/Table.js
+++ frontend/src/Table.js
@@ -26,15 +26,33 @@ function checkCategories(categoriesChecker){
     return categories;
 }
 
+function setTableWidth(categories){
+    let width = 40;
+    categories.map(category => {
+        if (category == "Student mail")
+            width += 400;
+        else
+            width += 160;
+    });
+
+    console.log(width.toString() + "px");
+
+    document.getElementById('d-table').style.width = (width+2).toString() + "px";
+    document.getElementById('table-header-row').style.width = width.toString() + "px";
+    document.getElementById('table-body').style.width = (width+2).toString() + "px";
+}
+
 const Table = props => {
     if (props.data.length === 0) return;
 
     const categories = checkCategories(props.categories);
     if (categories.length === 0) return;
 
+    setTableWidth(categories);
+
     return (
         <div id="table">
-            <table>
+            <table id="d-table">
                 <thead id="table-header-row">
                     <tr >
                         <TableHeader categories={categories} />
